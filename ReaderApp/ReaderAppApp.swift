444f11303a360d24bfea0e7248f40651e14ab7f9
steffds <sdesmedt7@gmail.com>
2022-08-18 10:16:57 +0200

    Initial Commit

---
+++ ReaderApp/ReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderAppApp.swift
+//  ReaderApp
+//
+//  Created by Stephanie De Smedt on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
